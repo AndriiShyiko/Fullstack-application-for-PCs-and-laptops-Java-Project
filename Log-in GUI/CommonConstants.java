ba60c5d22a29013d7f0523f3f7ed7d377322240e
Andrii Shyiko <andriishiiko@gmail.com>
2026-03-31 11:58:22 +0100

    Add files via upload

---
+++ Log-in GUI/CommonConstants.java
@@ -0,0 +1,11 @@
+package ans;
+
+import java.awt.*;
+
+public class CommonConstants 
+{
+    //color hex values
+    public static final Color PRIMARY_COLOR = Color.decode("#191E29");
+    public static final Color SECONDARY_COLOR = Color.decode("#132d46");
+    public static final Color TEXT_COLOR = Color.decode("#01C38D");
+}
